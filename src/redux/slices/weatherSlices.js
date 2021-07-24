84828d4cb4a5b19cc9eacfd781bb2026d57c786a
Emmanuel <twentekghana@gmail.com>
2021-07-24 21:30:46 +0800

    weather-app

--- src/redux/slices/weatherSlices.js
+++ src/redux/slices/weatherSlices.js
@@ -6,7 +6,7 @@ export const fetchWeatherAction = createAsyncThunk(
   async (payload, { rejectWithValue, getState, dispatch }) => {
     try {
       const { data } = await axios.get(
-        `http://api.openweathermap.org/data/2.5/weather?q=${payload}&appid=${process.env.REACT_APP_OPEN_WEATHER_KEY}&units=imperial`
+        `http://api.openweathermap.org/data/2.5/weather?q=${payload}&appid=094b545dde613af5667ba10639a224f8&units=imperial`
       );
 
       return data;
